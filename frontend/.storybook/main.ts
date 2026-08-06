245500d062d3049305e9bf1457fbdc9987375eae
Abhishek Maurya <mauryaabhishek279@gmail.com>
2026-08-06 23:31:13 +0530

    fix: removed unused add-on

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -17,8 +17,7 @@ const config: StorybookConfig = {
   ],
   "addons": [
     getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('@storybook/addon-themes')
+    getAbsolutePath('@storybook/addon-a11y')
   ],
   "framework": getAbsolutePath('@storybook/nextjs-vite'),
   "staticDirs": [
