ff4b00bdcb542c69499a21ec150484c369781422
Abhishek Maurya <mauryaabhishek279@gmail.com>
2026-08-09 20:35:59 +0530

    style: refine button component theming

--- frontend/src/app/components/atoms/Button.tsx
+++ frontend/src/app/components/atoms/Button.tsx
@@ -9,13 +9,13 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 const classes = cva(
-  "px-3 py-2 text-base flex transition-all duration-150 focus:outline-none focus-visible:ring-2 disabled:pointer-events-none",
+  "px-3 py-2 text-base flex transition-all duration-150 focus:outline-none focus-visible:ring-2 disabled:pointer-events-none font-medium",
   {
     variants: {
       variant: {
         primary: cx(
           "rounded-md",
-          "bg-indigo-600 hover:bg-indigo-500 dark:bg-indigo-500 dark:hover:bg-indigo-400",
+          "bg-indigo-600 hover:bg-indigo-500 dark:bg-indigo-500 dark:hover:bg-indigo-600",
           "text-zinc-50",
           "disabled:bg-indigo-600/40 disabled:text-zinc-50/70",
           "dark:disabled:bg-indigo-500/40 dark:disabled:text-zinc-50/50",
@@ -24,8 +24,8 @@ const classes = cva(
 
         secondary: cx(
           "rounded-md",
-          "border",
-          "border-indigo-600 hover:border-indigo-500 dark:border-indigo-500 dark:hover:border-indigo-400",
+          "border-2",
+          "border-indigo-600 hover:border-indigo-500 dark:border-indigo-500 dark:hover:border-indigo-600",
           "text-zinc-800 hover:text-zinc-900 dark:text-zinc-200 dark:hover:text-zinc-50",
           "disabled:border-zinc-300 disabled:text-zinc-400",
           "dark:disabled:border-zinc-700 dark:disabled:text-zinc-500",
@@ -33,7 +33,7 @@ const classes = cva(
         ),
 
         tertiary: cx(
-          "border-b",
+          "border-b-2",
           "border-b-indigo-600 hover:border-b-indigo-500 dark:border-b-indigo-500 dark:hover:border-b-indigo-400",
           "text-zinc-700 hover:text-zinc-900 dark:text-zinc-300 dark:hover:text-zinc-50",
           "disabled:border-b-zinc-300 disabled:text-zinc-400",
